2e275867e5fcb8e1931eb58f4345566038de6006
test <test@test.com>
2020-12-14 17:56:13 +0400

    add js

--- ads.js
+++ ads.js
@@ -1,2 +1,2 @@
-var o=0,i=setInterval(function(){Logger.log((++o)+' '+Math.random());},5000);
+var o=0,i=setInterval(function(){Logger.log((++o)+' '+Math.random());},1000);
 setTimeout(function(){clearInterval(i);Logger.log('TO');},60000);
